fc24dd36d70dc0221300e4b2435abd3fa9cacc1f
Twan Hogervorst <twanhog@gmail.com>
2022-02-11 11:54:45 +0100

    Include IOT-components

--- Project/main/main.c
+++ Project/main/main.c
@@ -4,12 +4,18 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "mcp23017.h"
+
+#define LYRAT_SCK 23
+#define LYRAT_SDA 18
 
 #define LCD_I2C_ADDR = 0x27
 #define GPIO_I2C_ADDR = 0x20
 #define AUDIO_I2C_ADDR = 0x10
 
+
+
 void app_main(void)
 {
-
+    
 }
