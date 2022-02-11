ca3deb00451d6bf5ef75e448b97e22f22c6adb97
jasper baijens <jasperbayens@outlook.com>
2022-02-11 14:36:23 +0100

    Opdracht2&3 naam

---
+++ Project_2/main/main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "mcp23017.h"
+#include "i2c_bus.h"
+
+#define LYRAT_SCK 23
+#define LYRAT_SDA 18
+
+#define LYRAT_SCK_SPEED 10000
+
+#define LCD_I2C_ADDR 0x27
+#define GPIO_I2C_ADDR 0x20
+#define AUDIO_I2C_ADDR 0x10
+
+mcp23017_handle_t mcp23017;
+
+TaskHandle_t Task1;
+TickType_t lastWakeTime1;
+TaskHandle_t Task2;
+TickType_t lastWakeTime2;
+
+SemaphoreHandle_t buttonMutex;
+uint8_t buttonField = 0x00;
+
+void Rood (void * parameter) 
+{
+    lastWakeTime1 = xTaskGetTickCount();
+
+    while (1)
+    {
+
+        uint8_t gpioA = mcp23017_read_io(mcp23017, MCP23017_GPIOA);
+
+        char buttonValue = gpioA & 0x01;
+
+        xSemaphoreTake(buttonMutex, portMAX_DELAY);
+        
+        buttonField = buttonValue ? 
+            (buttonField | buttonValue) 
+            : (buttonField & ~buttonValue);
+
+        xSemaphoreGive(buttonMutex);
+
+        if(buttonField & 0x01) 
+        {
+            mcp23017_write_io(mcp23017, buttonField, MCP23017_GPIOB);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+            mcp23017_write_io(mcp23017, buttonField, MCP23017_GPIOB);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+        } else {
+            mcp23017_write_io(mcp23017, buttonField, MCP23017_GPIOB);
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void Geel (void * parameter) 
+{
+    lastWakeTime2 = xTaskGetTickCount();
+
+    while (1)
+    {
+        uint8_t gpioA = mcp23017_read_io(mcp23017, MCP23017_GPIOA);
+
+        char buttonValue = gpioA & 0x02;
+
+        xSemaphoreTake(buttonMutex, portMAX_DELAY);
+        
+        buttonField = buttonValue ? 
+            (buttonField | buttonValue) 
+            : (buttonField & ~buttonValue);
+
+        xSemaphoreGive(buttonMutex);
+
+        if(buttonField & 0x02) 
+        {
+            mcp23017_write_io(mcp23017, buttonField, MCP23017_GPIOB);
+            vTaskDelay(200 / portTICK_PERIOD_MS);
+            mcp23017_write_io(mcp23017, buttonField, MCP23017_GPIOB);
+            vTaskDelay(200 / portTICK_PERIOD_MS);
+        } else {
+            mcp23017_write_io(mcp23017, buttonField, MCP23017_GPIOB);
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    i2c_config_t i2c_conf = {
+        .mode = I2C_MODE_MASTER,
+        .scl_io_num = LYRAT_SCK,
+        .sda_io_num = LYRAT_SDA,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = LYRAT_SCK_SPEED
+    };
+
+    i2c_bus_handle_t i2c_bus = i2c_bus_create(I2C_NUM_0, &i2c_conf);
+    mcp23017 = mcp23017_create(i2c_bus, GPIO_I2C_ADDR);
+
+    mcp23017_set_io_dir(mcp23017, 0x01, MCP23017_GPIOA);
+    mcp23017_set_io_dir(mcp23017, 0x00, MCP23017_GPIOB);
+
+    buttonMutex = xSemaphoreCreateMutex();
+    
+    xTaskCreatePinnedToCore(Rood, "Task1", 10000, NULL, 6, &Task1, 1);
+    xTaskCreatePinnedToCore(Geel, "Task2", 10000, NULL, 6, &Task2, 1);
+}
